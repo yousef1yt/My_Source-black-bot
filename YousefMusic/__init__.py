529e084be06b184d6b2d1fb92edcaa636903e18a
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-10 08:19:51 +0300

    Add files via upload

---
+++ YousefMusic/__init__.py
@@ -0,0 +1,28 @@
+
+from YousefMusic.core.bot import Zelzaly
+from YousefMusic.core.dir import dirr
+from YousefMusic.core.git import git
+from YousefMusic.core.userbot import Userbot
+from YousefMusic.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Zelzaly()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
+YTB = YTM()
