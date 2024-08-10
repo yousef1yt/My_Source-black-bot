6766daf85c9460c68962af40a26079dc52746e8f
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-10 14:20:32 +0300

    Update logger.py

--- YousefMusic/utils/logger.py
+++ YousefMusic/utils/logger.py
@@ -1,36 +1,35 @@
-from config import LOGGER, LOGGER_ID
-from YousefMusic import app
-from YousefMusic.utils.database import is_on_off
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ ʑᴇʟᴢᴀʟ_ᴍᴜsɪᴄ ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/ZThon   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/Zelzal_Music ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+from pyrogram.enums import ParseMode
+
+from ZelzalMusic import app
+from ZelzalMusic.utils.database import is_on_off
+from config import LOGGER_ID
 
 
 async def play_logs(message, streamtype):
-    if await is_on_off(LOGGER):
-        if message.chat.username:
-            chatusername = f"@{message.chat.username}"
-        else:
-            chatusername = "Private Group"
-        if message.from_user:
-            useri = message.from_user.id
-            users = f"@{message.from_user.username}"
-            user = message.from_user.mention
-        else:
-            useri = "Channel Player"
-            users = "Channel Player"
-            user = "Channel Player"
+    if await is_on_off(2):
         logger_text = f"""
-**• بدا تشغيل اغنيه من البوت 🤖**
-**• الجروب ->** {message.chat.title} [`{message.chat.id}`]
-**• اسم المستخدم ->** {user}
-**• يوزر المستخدم ->** {users}
-**• ايدي المستخدم ->** {useri}
-**• لينك الجروب ->** {chatusername}
-**• نوع التشغيل ->** {message.text}
-**• نوع البث ->** {streamtype}"""
+<b>- هنـاك شخص يستخـدم الميـوزك حاليـاً 🎻</b>
+
+<b>- الاسم :</b> {message.from_user.mention}
+<b>- اليوزر :</b> @{message.from_user.username}
+<b>- ايدي المستخدم :</b> <code>{message.from_user.id}</code>
+
+<b>- اسم المجموعة :</b> {message.chat.title}
+<b>- يوزر المجموعة :</b> @{message.chat.username}
+<b>- ايدي المجموعة :</b> <code>{message.chat.id}</code>
+
+<b>- الطلب :</b> {message.text.split(None, 1)[1]}
+<b>- نوع التشغيل :</b> {streamtype}"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
-                    LOGGER_ID,
-                    f"{logger_text}",
+                    chat_id=LOGGER_ID,
+                    text=logger_text,
+                    parse_mode=ParseMode.HTML,
                     disable_web_page_preview=True,
                 )
             except:
