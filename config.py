06d83f4b67447753c8bf6e5bc260cebca920bed3
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-10 08:42:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,10 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/yousef1yt/My_Source-black-bot")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN")
-CH_US = getenv("CH_US", "cecrr") # Fill this variable if your upstream repository is private
+CH_US = getenv("CH_US", "cecrr")
+CHANNEL_SUDO = getenv("CHANNEL_SUDO", "ceccr")
+YAFA_NAME = getenv("YAFA_NAME", "اضغط هنا للاشترااك")
+YAFA_CHANNEL = getenv("YAFA_CHANNEL", "https://t.me/cecrr")# Fill this variable if your upstream repository is private
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/P_6_B")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/cecrr")
 
