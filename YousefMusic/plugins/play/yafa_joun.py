b52bc9262d45c63407e85647f388209cd1eb4edc
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-10 08:41:24 +0300

    Update yafa_joun.py

--- YousefMusic/plugins/play/yafa_joun.py
+++ YousefMusic/plugins/play/yafa_joun.py
@@ -3,7 +3,7 @@ from strings.filters import command
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
 from config import YAFA_CHANNEL, YAFA_NAME, CHANNEL_SUDO
-from AlexaMusic import app
+from YousefMusic import app
 
 
 @app.on_message(filters.incoming & filters.private, group=-1)
@@ -21,7 +21,7 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"**🎗︙عذࢪاً عمࢪي {msg.from_user.mention} انت غيࢪ مشتࢪك بقناه تحديثات البوت △**\n**🎗︙اشترك ثم ارسل** /start",
+                    f"🎗︙عذࢪاً عمࢪي {msg.from_user.mention} انت غيࢪ مشتࢪك بقناه تحديثات البوت △**\n**🎗︙اشترك ثم ارسل /start",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton(f"{YAFA_NAME}", url=link)]
