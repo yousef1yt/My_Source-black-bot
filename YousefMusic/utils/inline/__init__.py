85dfdaddb0df797b993c21318aab715639982762
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-11 16:16:36 +0300

    Update __init__.py

--- YousefMusic/utils/inline/__init__.py
+++ YousefMusic/utils/inline/__init__.py
@@ -12,7 +12,6 @@ as you want or you can collabe if you have new ideas.
 
 from .help import *
 from .play import *
-from .playlist import *
 from .queue import *
 from .settings import *
 from .song import *
