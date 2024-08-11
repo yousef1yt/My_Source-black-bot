8394d111ff0fc3b2b9fa8386d186502cf2114ae3
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-11 16:30:42 +0300

    Update __init__.py

--- YousefMusic/utils/inline/__init__.py
+++ YousefMusic/utils/inline/__init__.py
@@ -1,20 +1,8 @@
-# Copyright (C) 2024 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-""""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2024 -present Team=Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
-
-from .help import *
 from .extras import *
-from .speed import *
 from .help import *
 from .play import *
 from .queue import *
 from .settings import *
+from .speed import *
 from .start import *
+from .song import *
