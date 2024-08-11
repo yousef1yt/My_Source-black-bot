f35b9eefdab1d4c46d776339c6cc918068d21e27
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-11 08:34:36 +0300

    Update AX.py

--- YousefMusic/plugins/play/AX.py
+++ YousefMusic/plugins/play/AX.py
@@ -1,6 +1,8 @@
 from pyrogram import filters, Client
 from YousefMusic import app
 import asyncio
+import config
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.types import VideoChatEnded, Message
 from pytgcalls import PyTgCalls, StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
@@ -8,78 +10,55 @@ from YousefMusic.core.call import Zelzaly
 from YousefMusic.utils.database import *
 from pytgcalls.exceptions import (NoActiveGroupCall,TelegramServerError,AlreadyJoinedError)
 
+
 @app.on_message(filters.regex("^مين في الكول$"))
 async def strcall(client, message):
-    assistant = await group_assistant(Zelzaly,message.chat.id)
+    assistant = await group_assistant(Zelzaly, message.chat.id)
     try:
-        await assistant.join_group_call(message.chat.id, AudioPiped("./YousefMusic/assets/call.mp3"), stream_type=StreamType().pulse_stream)
-        text=" الموجودين في الكول:\n\n"
+        await assistant.join_group_call(message.chat.id, AudioPiped("https://graph.org/file/217aac5f9cd2b05f7ba5a.mp4"), stream_type=StreamType().pulse_stream)
+        text = "~ المتكلمين بالأتصال :\n\n"
         participants = await assistant.get_participants(message.chat.id)
-        k =0
+        k = 0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut="يتكلم "
+                mut = "عم يحكي "
             else:
-                mut="ساكت "
+                mut = "مسكر المايك "
             user = await client.get_users(participant.user_id)
-            k +=1
-            text +=f"{k}➤{user.mention}➤{mut}\n"
-        text += f"\nعددهم : {len(participants)}\n"    
-        await message.reply(f"{text}")
+            k += 1
+            text += f"{k} ~ {user.mention} {mut}\n"
+        text += f"\n~ عددهم : {len(participants)}"  
+
+        # إضافة زر شفاف في الأسفل
+        inline_keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton(". 𝖠 ' 𝟣𝟣𝟣 .", url=config.SUPPORT_CHAT)],
+        ])      
+
+        await message.reply(f"{text}", reply_markup=inline_keyboard)
         await asyncio.sleep(7)
         await assistant.leave_group_call(message.chat.id)
     except NoActiveGroupCall:
-        await message.reply(f"المكالمه مش مفتوحه اصلا")
+        await message.reply(f"- ماكو شي مشتغل .")
     except TelegramServerError:
-        await message.reply(f"ارسل الامر تاني في مشكله في سيرفر التلجرام")
+        await message.reply(f"- حدث خطأ .")
     except AlreadyJoinedError:
-        text=" الموجودين في الكول:\n\n"
+        text = "~ المتكلمين بالأتصال :\n\n"
         participants = await assistant.get_participants(message.chat.id)
-        k =0
+        k = 0
         for participant in participants:
             info = participant
             if info.muted == False:
-                mut="يتكلم"
+                mut = "عم يحكي "
             else:
-                mut="ساكت "
+                mut = "مسكر المايك "
             user = await client.get_users(participant.user_id)
-            k +=1
-            text +=f"{k}➤{user.mention}➤{mut}\n"
-        text += f"\nعددهم : {len(participants)}\n"    
-        await message.reply(f"{text}")
-@app.on_message(filters.video_chat_ended)
-async def brah2(client, message):
-    da = message.video_chat_ended.duration
-    ma = divmod(da, 60)
-    ho = divmod(ma[0], 60)
-    day = divmod(ho[0], 24)
-    if da < 60:
-       await message.reply(f"** - تم انهاء مكالمة الفيديو مدتها {da} ثواني وصكرها **")        
-    elif 60 < da < 3600:
-        if 1 <= ma[0] < 2:
-            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها دقيقه**")
-        elif 2 <= ma[0] < 3:
-            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها دقيقتين **")
-        elif 3 <= ma[0] < 11:
-            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها {ma[0]} دقايق **")  
-        else:
-            await message.reply(f"**- تم إنهاء مكالمة الفيديو مدتها {ma[0]} دقيقه**")
-    elif 3600 < da < 86400:
-        if 1 <= ho[0] < 2:
-            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها ساعه **")
-        elif 2 <= ho[0] < 3:
-            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها ساعتين **")
-        elif 3 <= ho[0] < 11:
-            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها {ho[0]} ساعات **")  
-        else:
-            await message.reply(f"**- تم إنهاء مكالمة الفيديو مدتها {ho[0]} ساعة **")
-    else:
-        if 1 <= day[0] < 2:
-            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها يوم **")
-        elif 2 <= day[0] < 3:
-            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها يومين **")
-        elif 3 <= day[0] < 11:
-            await message.reply(f"**- تم انهاء مكالمة الفيديو مدتها {day[0]} ايام **")  
-        else:
-            await message.reply(f"**- تم إنهاء مكالمة الفيديو مدتها {day[0]} يوم**")
+            k += 1
+            text += f"{k} ~ {user.mention} {mut}\n"
+        text += f"\n~ عددهم : {len(participants)}"
+
+        # إضافة زر شفاف في الأسفل
+        inline_keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton(". 𝖠 ' 𝟣𝟣𝟣 .", url=config.SUPPORT_CHAT)],
+        ])
+        await message.reply(f"{text}", reply_markup=inline_keyboard)
