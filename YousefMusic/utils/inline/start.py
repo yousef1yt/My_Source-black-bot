35ac23b457e532ee2749d18c26e69542c6b549dc
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-11 20:33:17 +0300

    Update start.py

--- YousefMusic/utils/inline/start.py
+++ YousefMusic/utils/inline/start.py
@@ -2,7 +2,7 @@
 from YousefMusic import app
 from typing import Union
 from pyrogram.types import InlineKeyboardButton
-from config import SUPPORT_CHANNEL, SUPPORT_CHAT, OWNER_ID
+from config import YAFA_CHANNEL, SUPPORT_CHAT, OWNER_ID
 
 
 def start_panel(_):
@@ -15,7 +15,7 @@ def start_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
-            InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
+            InlineKeyboardButton(text=_["S_B_4"], url=f"{YAFA_CHANNEL}"),
         ],
         [
             InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_CHAT}"),
@@ -36,7 +36,7 @@ def private_panel(_, OWNER_ID: Union[bool, int] = None):
     buttons = [
         [InlineKeyboardButton(text=_["S_B_8"], callback_data="zzzback")],
         [
-            InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
+            InlineKeyboardButton(text=_["S_B_4"], url=f"{YAFA_CHANNEL}"),
             InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_CHAT}"),
         ],
         [
