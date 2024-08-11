9162758243ad4b454bb81ab7e9c0038e3e345c4d
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-11 19:33:56 +0300

    Update start.py

--- YousefMusic/utils/inline/start.py
+++ YousefMusic/utils/inline/start.py
@@ -27,7 +27,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text="‹ مطور السورس ›", url=f"https://t.me/y_o_v"
             ),
-        ],
+        ]
     ]
     return buttons
 
