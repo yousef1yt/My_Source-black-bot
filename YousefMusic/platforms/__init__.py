d73db405b8a67eb8991b7038e1526e428d510ff5
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-13 17:21:20 +0300

    Update __init__.py

--- YousefMusic/platforms/__init__.py
+++ YousefMusic/platforms/__init__.py
@@ -4,7 +4,6 @@
 
 from .Apple import AppleAPI
 from .Carbon import CarbonAPI
-from .Resso import RessoAPI
 from .Soundcloud import SoundAPI
 from .Spotify import SpotifyAPI
 from .Telegram import TeleAPI
