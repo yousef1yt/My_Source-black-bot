f47e33763473826c31e86d2aa82d5546cdbbf4ae
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-13 19:16:50 +0300

    Update __init__.py

--- YousefMusic/__init__.py
+++ YousefMusic/__init__.py
@@ -19,8 +19,12 @@ app = Zelzaly()
 userbot = Userbot()
 
 
+from .platforms import *
 
 Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
 Spotify = SpotifyAPI()
+Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
