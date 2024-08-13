58cae3c87edb29ba15e1c0d9566ae4986558ade6
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-13 21:20:41 +0300

    Update start.py

--- YousefMusic/utils/inline/start.py
+++ YousefMusic/utils/inline/start.py
@@ -1,72 +1,78 @@
+#
+# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
 
-from YousefMusic import app
 from typing import Union
+
 from pyrogram.types import InlineKeyboardButton
-from config import YAFA_CHANNEL, SUPPORT_CHAT, OWNER_ID
+
+from config import YAFA_CHANNEL, OWNER_CHANNEL, SUPPORT_CHAT
+from YousefMusic import app
 
 
-def start_panel(_):
+def start_pannel(_):
     buttons = [
         [
             InlineKeyboardButton(
                 text=_["S_B_1"],
-                url=f"https://t.me/{app.username}?start=help",
-            )
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
-            InlineKeyboardButton(text=_["S_B_4"], url=f"{YAFA_CHANNEL}"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_CHAT}"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/y_o_v"),
-        ] if OWNER_ID else [],
-        [InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")],
-            
-        ],
-    ]
-    return buttons
-
-
-def private_panel(_, OWNER_ID: Union[bool, int] = None):
-    buttons = [
-        [InlineKeyboardButton(text=_["S_B_8"], callback_data="zzzback")],
-        [
-            InlineKeyboardButton(text=_["S_B_4"], url=f"{YAFA_CHANNEL}"),
-            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_CHAT}"),
-        ],
-        [
-            InlineKeyboardButton(
-                text=_["S_B_5"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/y_o_v"),
-        ] if OWNER_ID else [],
-        [InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")],
-    
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_6"], url=f"https://t.me/y_o_v"),
-        ] if OWNER_ID else [],
-        [InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")],
-    ]
-    return buttons
-
-
-def alive_panel(_):
-    buttons = [
-        [
-            InlineKeyboardButton(
-                text="- اضفني .", url=f"https://t.me/{app.username}?startgroup=true"
+                url=f"https://t.me/{app.username}?start=help",",
             ),
-            InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_CHAT}"),
+            InlineKeyboardButton(
+                text=_["S_B_2"], callback_data="settings_helper"
+            ),
+        ],
+    ]
+    if YAFA_CHANNEL and SUPPORT_CHAT:
+        buttons.append(
+            [
+                InlineKeyboardButton(
+                    text=_["S_B_4"], url=f"{YAFA_CHANNEL}"
+                ),
+                InlineKeyboardButton(
+                    text=_["S_B_3"], url=f"{SUPPORT_CHAT}"
+                ),
+            ]
+        )
+    else:
+        if YAFA_CHANNEL:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text=_["S_B_4"], url=f"{YAFA_CHANNEL}"
+                    )
+                ]
+            )
+        if SUPPORT_CHAT:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text=_["S_B_3"], url=f"{SUPPORT_CHAT}"
+                    )
+                ]
+            )
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_8"],
+                callback_data=f"zzzback",
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_6"], url=f"{OWNER_CHANNEL}"
+            ),
+        ],
+        [
+                    InlineKeyboardButton(
+                        text=_["S_B_7"], user_id=OWNER
+            ),
         ],
     ]
     return buttons
