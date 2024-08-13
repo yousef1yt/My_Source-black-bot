8b94b65e37ca8e7c44e8335f24f67ad4e87d883a
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-13 17:24:53 +0300

    Update __init__.py

--- YousefMusic/platforms/__init__.py
+++ YousefMusic/platforms/__init__.py
@@ -3,8 +3,6 @@
 #▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/Zelzal_Music ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
 
 from .Apple import AppleAPI
-from .Carbon import CarbonAPI
-from .Soundcloud import SoundAPI
 from .Spotify import SpotifyAPI
 from .Telegram import TeleAPI
 from .Youtube import YouTubeAPI
