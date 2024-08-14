44ce3780730afb7cc408768f4d9a5e7b7ff275e2
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-14 09:34:00 +0300

    Update git.py

--- YousefMusic/core/git.py
+++ YousefMusic/core/git.py
@@ -47,9 +47,9 @@ def git():
         UPSTREAM_REPO = config.UPSTREAM_REPO
     try:
         repo = Repo()
-        LOGGER("ميــوزك الـجـزار").info(f"Git Client Found [VPS DEPLOYER]")
+        LOGGER("ميــوزك اكس").info(f"Git Client Found [VPS DEPLOYER]")
     except GitCommandError:
-        LOGGER("ميــوزك الـجـزار").info(f"Invalid Git Command")
+        LOGGER("ميــوزك اكس").info(f"Invalid Git Command")
     except InvalidGitRepositoryError:
         repo = Repo.init()
         if "origin" in repo.remotes:
@@ -76,4 +76,4 @@ def git():
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
         install_req("pip3 install --no-cache-dir -r requirements.txt")
-        LOGGER("ميــوزك الـجـزار").info(f"جارِ الكشف عن تحديثات جديدة . . .")
+        LOGGER("ميــوزك اكس").info(f"جارِ الكشف عن تحديثات جديدة . . .")
