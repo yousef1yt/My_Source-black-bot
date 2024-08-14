1ed546ebffa2c1bcf60365b9c7e6fae5665aea53
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-14 08:06:42 +0300

    Update queue.py

--- YousefMusic/utils/inline/queue.py
+++ YousefMusic/utils/inline/queue.py
@@ -70,7 +70,7 @@ def queue_back_markup(_, CPLAY):
 
 
 def aq_markup(_, chat_id):
-    chat_tit = "Not ᥉ꪮ᥉."
+    chat_tit = ". 𝖠 ' 𝟣𝟣𝟣 ."
     buttons = [
         [InlineKeyboardButton(text=chat_tit, url=f"https://t.me/{CH_US}")],
     ]
