939a6d7924b74ef791d8c6e574260551168df531
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-21 01:56:05 +0300

    Update play.py

--- YousefMusic/utils/inline/play.py
+++ YousefMusic/utils/inline/play.py
@@ -68,7 +68,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="ايقاف", callback_data=f"ADMIN Pause|{chat_id}"),
             
         ],[
-            InlineKeyboardButton(text="ᏟᎻᎪΝΝᎬᏞ", url=f"https://t.me/cecrr"),
+            InlineKeyboardButton(text=_["S_B_3"], url=config.YAFA_CHANNEL),
             InlineKeyboardButton(text="ᏟᎻᎪΝΝᎬᏞ²", url=f"https://t.me/P_6_B"),
            
         ],[
