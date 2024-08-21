dd30a047624cd6c8065c1f351fdbb1cafc7339cf
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-21 22:24:29 +0300

    Update devl.py

--- YousefMusic/plugins/play/devl.py
+++ YousefMusic/plugins/play/devl.py
@@ -1,5 +1,5 @@
-
 import asyncio
+
 import os
 import time
 import requests
@@ -9,36 +9,30 @@ import random
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from strings.filters import command
-from YousefMusic import (Apple, Resso, Spotify, Telegram, YouTube, app)
+from YousefMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from YousefMusic import app
 from random import  choice, randint
-
-                
 @app.on_message(
-    command(["سورس","• السورس •","السورس"])
-    
+    command(["صورص","سورس","السورس","• السورس •", "سبارك"])
 )
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/064fe0dc07a2cba072718.jpg",
-        caption=f"• 𝗧𝗵𝗲 𝗕𝗲𝘀𝘁 𝗦𝗼𝘂𝗿𝗰𝗲 𝗢𝗻 𝗧𝗲𝗹𝗲𝗴𝗮𝗺 . 𝖠 ' 𝟣𝟣𝟣 . 🎸 .",
+        photo=f"https://graph.org/file/2577f47589c4b4c63e4a6.jpg",
+        caption=f"""
+  - 𓏺[𝙎𝙊𝙐𝙍𝘾𝙀 . 𝖠 ' 𝟣𝟣𝟣 .](https://t.me/cecrr) 
+—————————————
+-   [𝙗𝙤𝙩 𝙘𝙝𝙖𝙩 𝗕𝗹𝗮𝗰𝗸 .](https://t.me/F6Z_bot) 
+
+-  [𝙨𝙤𝙪𝙧𝙘𝙚 𝙙𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧](https://t.me/y_o_v) 
+""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "𖥻 𝗖𝗛𝗔𝗡𝗘𝗟 .", url=f"https://t.me/cecrr"), 
-                 InlineKeyboardButton(
-                   "‹ 𝗬𝗼𝘂𝘀𝗲𝗳 ›",       url=f"https://t.me/y_o_v"), 
-                 
-             
-             ],[ 
-            InlineKeyboardButton(
-                      "‹ لتنصيب بوت مماثل ›", url=f"https://t.me/y_o_v"), 
-                      
-             ],[ 
-                  InlineKeyboardButton(
-                text="𖥻 AdD Me To YoUr GrOuP .",
-                url=f"https://t.me/{app.username}?startgroup=true"),
+                        "⛥ 𓏺 َ𝗬𝗼𝘂𝘀𝗲𝗳 .", url=f"https://t.me/cecrr"), 
+                ],[
+                    InlineKeyboardButton(
+                        ". 𝖠 ' 𝟣𝟣𝟣 .", url=f"t.me/cecrr"),
                 ],
 
             ]
@@ -48,28 +42,33 @@ async def huhh(client: Client, message: Message):
     )
 
 
-@app.on_message(filters.command(["قتهبهبه السورس","تبهبتبعبع","نلهلهلهل","بهبتعبعب السورس ✯"], ""), group=73) 
-async def deev(client: Client, message: Message):
-     user = await client.get_chat(chat_id="y_o_v")
-     name = user.first_name
-     username = user.username 
-     bio = user.bio
-     user_id = user.id
-     photo = user.photo.big_file_id
-     photo = await client.download_media(photo)
-     link = f"https://t.me/{message.chat.username}"
-     title = message.chat.title if message.chat.title else message.chat.first_name
-     chat_title = f"User : {message.from_user.mention} \nChat Name : {title}" if message.from_user else f"Chat Name : {message.chat.title}"
-     try:
-      await client.send_message(username, f"هناك شخص بالحاجه اليك عزيزي المطور\n{chat_title}\nChat Id : `{message.chat.id}`",
-      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{title}", url=f"{link}")]]))
-     except:
-       pass
-     await message.reply_photo(
-     photo=photo,
-     caption=f"𝙳𝚎𝚟𝚎𝚕𝚘𝚙𝚎𝚛 𝙽𝚊𝚖𝚎 : {name} \n𝚍𝚎𝚟 𝚞𝚜𝚎𝚛 𝚗𝚊𝚖𝚎 : @{username}\n{bio}",
-     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{name}", user_id=f"{user_id}")]]))
-     try:
-       os.remove(photo)
-     except:
-        pass
+
+@app.on_message(command(["غنيلي", "غني", "غ", "سبارك غنيلي"]))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(3,267)
+    url = f"https://t.me/bsmaatt/{rl}"
+    await client.send_voice(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الاغـنـية لـك",parse_mode="html",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
+    
+@app.on_message(command(["صورة","صور"]))
+async def ihd(client: Client, message: Message):
+    rs = random.randint(39,148)
+    url = f"https://t.me/GTTUTY/{rs}"
+    await client.send_photo(message.chat.id,url,caption="💕 ¦ تـم اختيـار الصوره لـك",parse_mode="html",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
