393fcebda0c4c1f0459d6e1476559f3fa99297d2
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-21 22:31:00 +0300

    Update cmds.py

--- YousefMusic/plugins/play/cmds.py
+++ YousefMusic/plugins/play/cmds.py
@@ -1,50 +1,209 @@
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ ʑᴇʟᴢᴀʟ_ᴍᴜsɪᴄ ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/ZThon   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/Zelzal_Music ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
-
 import asyncio
 import os
+from pyrogram.types import CallbackQuery
+from YousefMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from YousefMusic import app
 import requests
 import pyrogram
-from pyrogram import Client, filters, emoji
+from pyrogram import Client, emoji 
+from config import *
+from pyrogram import filters
 from strings.filters import command
+from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from pyrogram.errors import MessageNotModified
-from YousefMusic import app
-from config import OWNER_ID, LOGGER_ID
 
 
-@app.on_message(command(["ميوزك", "الميوزك", "الاوامر"]))
-async def zdatsr(client: Client, message: Message):
-    usr = await client.get_users(OWNER_ID)
-    name = usr.first_name
-    usrnam = usr.username
+
+@app.on_message(
+    command("الاوامر")
+)
+async def سبارك_source(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://te.legra.ph/file/ac75b0aedc730253e30e5.jpg",
-        caption=f"""<b>» مرحبـاً بك عـزيـزي </b> {message.from_user.mention} .\n\n<b>» استخـدم الازرار بالاسفـل ⚡\n» لـ تصفـح اوامـر سورس اكس</b>""",
+        photo=f"https://graph.org/file/2577f47589c4b4c63e4a6.jpg",
+        caption=f"""**⩹━★⊷━⌞ 𓏺َِ᥉َِ᥆َِꪊَِᖇَِᥴُِ꧖ َِ᥉َِρُِꪖَِᖇَِᥴُِƙَِ⌝⌯⊶★━⩺ ⌝━⊶★━⩺**\nمرحبا بك عزيزي {message.from_user.mention}\nهذا قسم الاوامر الخاص بسورس سبارك \nلمعرفة الاوامر اضغط على الأزرار بالأسفل👇\n**⩹━★⊷━⌞𓏺َِ᥉َِ᥆َِꪊَِᖇَِᥴُِ꧖ َِ᥉َِρُِꪖَِᖇَِᥴُِƙَِ⌝⌯⊶★━⩺⌝━⊶★━⩺**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "• اوامــر التشغيــل •", callback_data="zzzll"),
+                        "اوامر الجروبات", callback_data="gr"),
+                    InlineKeyboardButton(
+                        "اوامر القنوات", callback_data="ch"),  
+                 ],[
+                    InlineKeyboardButton(
+                        "اوامر الادمن", callback_data="adm"), 
                 ],[
+                
                     InlineKeyboardButton(
-                        "• اوامـر القنـاة •", callback_data="zzzch"),
-                    InlineKeyboardButton(
-                        "• اوامـر الادمـن •", callback_data="zzzad"),
-                ],[
-                    InlineKeyboardButton(
-                       "• اوامــر المطــور •", callback_data="zzzdv"),
-                    InlineKeyboardButton(
-                        "رجـوع", callback_data="zzzback"),
-                ],[
-                    InlineKeyboardButton(name, url=f"https://t.me/{usrnam}"),
-                ],[
-                    InlineKeyboardButton(
-                        ". 𝖠 ' 𝟣𝟣𝟣 .", url="https://t.me/cecrr"),
+                        ". 𝖠 ' 𝟣𝟣𝟣 .", url=f"https://t.me/cecrr"),
                 ],
+
             ]
+
         ),
+
     )
 
+    
+@app.on_callback_query(filters.regex("gr"))
+async def سبارك_usage(_, callback_query: CallbackQuery):
+    await callback_query.answer()
+    await callback_query.message.edit_text(
+        text="""**⌯⌞𓏺َِ᥉َِ᥆َِꪊَِᖇَِᥴُِ꧖ َِ᥉َِρُِꪖَِᖇَِᥴُِƙَِ⌝⌯⊶★━⩺⌝⌯⊶★━⩺**
+★¦ اهلا بك عزيزي في قسم اوامر التشغيل في الجروبات
+★¦ تشغيل + اسم الاغنيه
+★¦ فديو + اسم الاغنيه
+★¦ #فيديو + اسم الاغنيه
+★¦ #فديو + اسم الاغنيه
+★¦ {NAME_BOT} + اسم الاغنيه
+★¦ /فيديو + اسم الاغنيه
+★¦ /ق شغل + اسم الاغنيه
+★¦ /تشغيل + اسم الاغنيه
+★¦ cvplay + اسم الاغنيه
+★¦ cplay + اسم الاغنيه
+★¦ /vplay + اسم الاغنيه
+★¦ /play + اسم الاغنيه
+★¦ #تشغيل + اسم الاغنيه
+★¦ فيديو + اسم الاغنيه
+★¦ سورة + اسم السورة 
+★¦ cvplayforce + اسم الاغنيه
+★¦ cplayforce + اسم الاغنيه
+★¦ vplayforce + اسم الاغنيه
+★¦ playforce + اسم الاغنيه
+★¦ /cvplay + اسم الاغنيه
+★¦ vplay + اسم الاغنيه
+★¦ play + اسم الاغنيه
+
+**⌯⌞𓏺َِ᥉َِ᥆َِꪊَِᖇَِᥴُِ꧖ َِ᥉َِρُِꪖَِᖇَِᥴُِƙَِ⌝⌯⊶★━⩺⌝⌯⊶★━⩺**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "التالي", callback_data="ch"), 
+                    
+                ],[
+                    InlineKeyboardButton(
+                        "الرئيسية", callback_data="back"), 
+                    
+                ]
+            ]
+        )
+    )
+
+@app.on_callback_query(filters.regex("ch"))
+async def سبارك_usage(_, callback_query: CallbackQuery):
+    await callback_query.answer()
+    await callback_query.message.edit_text(
+        text="""**⌯𓏺َِ᥉َِ᥆َِꪊَِᖇَِᥴُِ꧖ َِ᥉َِρُِꪖَِᖇَِᥴُِƙَِ⌝⌯⊶★━⩺⌝⌯⊶★━⩺**
+★¦ اهلا بك عزيزي في قسم اوامر التشغيل في القنوات
+★¦ شغل + اسم الاغنيه
+★¦ قناه + اسم الاغنيه
+★¦ مانو + اسم الاغنيه
+★¦ ق + اسم الاغنيه
+★¦ اغاني + اسم الاغنيه
+★¦ . + اسم الاغنيه
+★¦ / + اسم الاغنيه
+**⩹⌯⌞𓏺َِ᥉َِ᥆َِꪊَِᖇَِᥴُِ꧖ َِ᥉َِρُِꪖَِᖇَِᥴُِƙَِ⌝⌯⊶★━⩺⌝⌯⊶★━⩺**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "التالي", callback_data="adm"), 
+                    InlineKeyboardButton(
+                        "العودة", callback_data="gr"), 
+                ],[
+                    InlineKeyboardButton(
+                        "الرئيسية", callback_data="back"), 
+                    
+                ]
+            ]
+        )
+    )
+
+@app.on_callback_query(filters.regex("adm"))
+async def سبارك_usage(_, callback_query: CallbackQuery):
+    await callback_query.answer()
+    await callback_query.message.edit_text(
+        text="""**𓏺َِ᥉َِ᥆َِꪊَِᖇَِᥴُِ꧖ َِ᥉َِρُِꪖَِᖇَِᥴُِƙَِ⌝⌯⊶★━⩺⌝⌯⊶★━⩺**
+★¦ اهلا بك عزيزي في قسم اوامر تشغيل الادمن
+★¦ رفع ثانوي
+★¦ تنزيل ثانوي
+★¦ قائمة الثانويين
+★¦ رفع ادمن
+★¦ تنزيل ادمن
+★¦ قائمة الادمن
+★¦ حظر
+★¦ الغاء الحظر
+★¦ المحظورين
+★¦ حظر عام
+★¦ الغاء الحظر العام
+★¦ المحظورين عام
+★¦ اونلاين
+★¦ اذاعه
+★¦ تحديث
+★¦ logger
+★¦ ريلود
+★¦ وقف
+★¦ كمل
+★¦ اسكت
+★¦ اتكلم
+★¦ ايقاف
+★¦ تخطي
+★¦ @all
+★¦ all stop
+★¦ يوتيوب / تنزيل
+★¦ playing
+★¦ القائمه
+★¦ حذف القائمه
+★¦ تحديث
+★¦ الاحصائيات
+★¦ لايف
+★¦ مساعده
+★¦ الاعدادت
+★¦ بينج
+
+**⩹━★⊷⌯⌞𓏺َِ᥉َِ᥆َِꪊَِᖇَِᥴُِ꧖ َِ᥉َِρُِꪖَِᖇَِᥴُِƙَِ⌝⌯⊶★━⩺ ⌝⌯⊶★━⩺**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "التالي", callback_data="gr"), 
+                    InlineKeyboardButton(
+                        "العودة", callback_data="ch"), 
+                ],[
+                    InlineKeyboardButton(
+                        "الرئيسية", callback_data="back"), 
+                    
+                ]
+            ]
+        )
+    )
+
+    
+@app.on_callback_query(filters.regex("back"))
+async def سبارك_back(_, callback_query: CallbackQuery):
+    await message.reply_photo(
+        photo=f"https://graph.org/file/2577f47589c4b4c63e4a6.jpg",
+        caption=f"""**⩹━★⊷━⌞𓏺َِ᥉َِ᥆َِꪊَِᖇَِᥴُِ꧖ َِ᥉َِρُِꪖَِᖇَِᥴُِƙَِ⌝⌯⊶★━⩺ ⌝━⊶★━⩺**\nمرحبا بك عزيزي {message.from_user.mention}\nهذا قسم الاوامر الخاص بسورس سبارك \nلمعرفة الاوامر اضغط على الأزرار بالأسفل👇\n**⩹━★⊷━⌞𓏺َِ᥉َِ᥆َِꪊَِᖇَِᥴُِ꧖ َِ᥉َِρُِꪖَِᖇَِᥴُِƙَِ⌝⌯⊶★━⩺ ⌝━⊶★━⩺**""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "اوامر الجروبات", callback_data="gr"),
+                    InlineKeyboardButton(
+                        "اوامر القنوات", callback_data="ch"),  
+                 ],[
+                    InlineKeyboardButton(
+                        "اوامر الادمن", callback_data="adm"), 
+                ],[
+                
+                    InlineKeyboardButton(
+                        "★⌞𓏺َِ᥉َِ᥆َِꪊَِᖇَِᥴُِ꧖ َِ᥉َِρُِꪖَِᖇَِᥴُِƙَِ⌝⌯⊶★━⩺⌝⚡", url=f"https://t.me/ZZZ7iZ"),
+                ],
+
+            ]
+
+        ),
+
+    )
 
