603cb40c3539d83e0e9ac495515eacd00cbdd023
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-21 09:21:12 +0300

    Update devl.py

--- YousefMusic/plugins/play/devl.py
+++ YousefMusic/plugins/play/devl.py
@@ -15,7 +15,7 @@ from random import  choice, randint
 
                 
 @app.on_message(
-    command(["سورس","سورس اكس","السورس"])
+    command(["سورس","• السورس •","السورس"])
     
 )
 async def huhh(client: Client, message: Message):
