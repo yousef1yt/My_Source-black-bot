c31462d3765efc7767e7dc5f4906ff9cc115e606
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-23 10:32:58 +0300

    Update play.py

--- YousefMusic/plugins/play/play.py
+++ YousefMusic/plugins/play/play.py
@@ -44,7 +44,7 @@ async def must_join_channel(app, msg):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"•عذرا on}\n• أشتࢪك في قناة البوت أولاً.\n•  انت غير مشترك عليك الأشتراك في قناة البوت \n•قناة البوت • @{CHANNEL_SUDO} •",
+                    f"• أشتࢪك في قناة البوت أولاً.\n•  انت غير مشترك عليك الأشتراك في قناة البوت \n•قناة البوت • @{CHANNEL_SUDO} ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
                         [InlineKeyboardButton("• اضغط هنا للاشتراك •", url=link)]
