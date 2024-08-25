67e2de75f88060543c52259a69cc8f7e610c617a
𓏺𓏺 𝐘𝐨𝐮𝐬𝐞𝐟 𓏺𓏺 ⤦اݪ⃪ہۧۛ حٰــ⃪َـمـ⃪َؤيٓ <167021222+yousef1yt@users.noreply.github.com>
2024-08-25 10:22:33 +0300

    Update play.py

--- YousefMusic/utils/inline/play.py
+++ YousefMusic/utils/inline/play.py
@@ -1,5 +1,9 @@
 import math
 
+import config
+
+from YousefMusic import app 
+
 from pyrogram.types import InlineKeyboardButton
 
 from YousefMusic.utils.formatters import time_to_seconds
@@ -27,38 +31,48 @@ def track_markup(_, videoid, user_id, channel, fplay):
     return buttons
 
 
-def track_markup(_, videoid, user_id, channel, fplay):
+def stream_markup_timer(_, chat_id, played, dur):
+    played_sec = time_to_seconds(played)
+    duration_sec = time_to_seconds(dur)
+    percentage = (played_sec / duration_sec) * 100
+    umm = math.floor(percentage)
+
     buttons = [
-        [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
+                [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
-        ],
-        [InlineKeyboardButton(name, user_id=id)],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        ],[  
+            InlineKeyboardButton(text="انهاء", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="استكمال", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="ايقاف", callback_data=f"ADMIN Pause|{chat_id}"),
+            
+        ],[
+            InlineKeyboardButton(text=_["S_B_9"], url=config.YAFA_CHANNEL),
+            
+        ],[
+            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID),
+        ],[
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
 
-def stream_markup(_, chat_id, name, id):
+def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [InlineKeyboardButton(name, user_id=id)],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+            InlineKeyboardButton(text="انهاء", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="استكمال", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="ايقاف", callback_data=f"ADMIN Pause|{chat_id}"),
+            
+        ],[
+            InlineKeyboardButton(text=_["S_B_9"], url=config.YAFA_CHANNEL),
+           
+        ],[
+            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID),
+        ],[
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -68,11 +82,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"ModyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"ModyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
@@ -126,7 +140,7 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
                 callback_data=f"forceclose {query}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="▷",
+                text="𝐑𝐄𝐒𝐔𝐌𝐄",
                 callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
             ),
         ],
