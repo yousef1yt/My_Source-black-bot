dbe3a6e7c706f3b98ca7b7e9674b4b3b8684d926
⛥ 𓏺 َ𝗬𝗼𝘂𝘀𝗲𝗳 . <167021222+yousef1yt@users.noreply.github.com>
2024-08-31 05:35:09 +0300

    Update Dev.py

--- YousefMusic/plugins/play/Dev.py
+++ YousefMusic/plugins/play/Dev.py
@@ -74,44 +74,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-OWNER_ID = 6009927981
-OWNER = getenv("OWNER")
-
-
-
-@app.on_message(filters.command(["مبرمج السورس", "• مبرمج السورس •", "الحكومه","بتبتبتتب"], ""))
-async def dev(client: Client, message: Message):
-     bot_username = client.me.username
-     user = await client.get_chat(6009927981)
-     name = user.first_name
-     username = user.username 
-     bio = user.bio
-     user_id = user.id
-     photo = user.photo.big_file_id
-     photo = await client.download_media(photo)
-     link = f"https://t.me/{message.chat.username}"
-     title = message.chat.title if message.chat.title else message.chat.first_name
-     chat_title = f"𓏺 𝖭𝖺𝗆𝖾 : {message.from_user.mention} \n 𓏺 𝖭𝖺𝗆𝖾 : {title}" if message.from_user else f"𝗂𝖣 : {message.chat.title}"
-     try:
-      await client.send_message(username, f"<b></b>\n حـد بينـادي عليك\n{chat_title}\nايدي الجروب : {message.chat.id}",
-      reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{title}", url=f"{link}")]]))
-     except:
-        pass
-     await message.reply_photo(
-     photo=photo,
-     caption=f"<b>•معلومات قلب يوسف• 𝖭𝖺𝗆𝖾 : {name}</b>\n<b>• 𝖴𝗌𝖾 : @{username}</b>\n<b>• 𝖡𝗂𝗈 : {bio}</b>",
-     reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"{name}", user_id=f"{user_id}")]]))
-     try:
-       os.remove(photo)
-     except:
-        pass
-import re
-import sys
-from os import getenv
-
-from dotenv import load_dotenv
-
-load_dotenv()
 
 OWNER_ID = getenv("OWNER_ID")
 OWNER = getenv("OWNER")
