0da722da5c82b9c20d79e55ff790f6cf01ca0f65
⛥ 𓏺 َ𝗬𝗼𝘂𝘀𝗲𝗳 . <167021222+yousef1yt@users.noreply.github.com>
2024-09-09 20:43:10 +0300

    Update hgra & wr2a % m2s.py

--- YousefMusic/plugins/play/hgra & wr2a % m2s.py
+++ YousefMusic/plugins/play/hgra & wr2a % m2s.py
@@ -48,7 +48,7 @@ def start(client, message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton("اضغط للعب", callback_data="join")],
-                    [InlineKeyboardButton("‹ . 𝖠 ' 𝟣𝟣𝟣 . ›", url="https://t.me/cecrr")]
+                    [InlineKeyboardButton("سورس ريال", url="https://t.me/SOURCE_RAEL")]
                 ]
             )
         )
@@ -67,7 +67,7 @@ def join(client, callback_query):
                         [InlineKeyboardButton("حجرة", callback_data="حجرة"),
                          InlineKeyboardButton("ورقة", callback_data="ورقة"),
                          InlineKeyboardButton("مقص", callback_data="مقص")],[
-                         InlineKeyboardButton("‹ . 𝖠 ' 𝟣𝟣𝟣 . ›", url="https://t.me/cecrr")
+                         InlineKeyboardButton("سورس ريال", url="https://t.me/SOURCE_RAEL")
                          ]
                     ]
                 )
@@ -94,7 +94,7 @@ def choose(client, callback_query):
                         [InlineKeyboardButton("حجرة", callback_data="حجرة"),
                          InlineKeyboardButton("ورقة", callback_data="ورقة"),
                          InlineKeyboardButton("مقص", callback_data="مقص")],
-                         [InlineKeyboardButton("‹ . 𝖠 ' 𝟣𝟣𝟣 . ›", url="https://t.me/cecrr")]
+                         [InlineKeyboardButton("سورس ريال", url="https://t.me/SOURCE_RAEL")]
                     ]
                 )
             )
