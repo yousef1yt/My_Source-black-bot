cf23b475943d9b9fd310de8c522702c7225f7f61
⛥ 𓏺 َ𝗬𝗼𝘂𝘀𝗲𝗳 . <167021222+yousef1yt@users.noreply.github.com>
2024-09-09 20:44:22 +0300

    Update AX.py

--- YousefMusic/plugins/play/AX.py
+++ YousefMusic/plugins/play/AX.py
@@ -32,7 +32,7 @@ async def strcall(client, message):
 
         # إضافة زر شفاف في الأسفل
         inline_keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton(". 𝖠 ' 𝟣𝟣𝟣 .", url=config.SUPPORT_CHAT)],
+            [InlineKeyboardButton("سورس ريال", url=config.SUPPORT_CHAT)],
         ])      
 
         await message.reply(f"{text}", reply_markup=inline_keyboard)
@@ -59,6 +59,6 @@ async def strcall(client, message):
 
         # إضافة زر شفاف في الأسفل
         inline_keyboard = InlineKeyboardMarkup([
-            [InlineKeyboardButton(". 𝖠 ' 𝟣𝟣𝟣 .", url=config.SUPPORT_CHAT)],
+            [InlineKeyboardButton("سورس ريال", url=config.SUPPORT_CHAT)],
         ])
         await message.reply(f"{text}", reply_markup=inline_keyboard)
