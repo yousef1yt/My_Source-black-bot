59a533ac1d909da501804cf55a118ddbebcb714e
⛥ 𓏺 َ𝗬𝗼𝘂𝘀𝗲𝗳 . <167021222+yousef1yt@users.noreply.github.com>
2024-09-25 01:15:22 +0300

    Update private.py

--- YousefMusic/plugins/play/private.py
+++ YousefMusic/plugins/play/private.py
@@ -4,11 +4,11 @@ from YousefMusic import app
 from pyrogram import filters
 from pyrogram.types import Message
 
-YOUR_OWNER_ID = 6094238403
+OWNER_
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 BOT_ID = 6827222674
 
-@app.on_message(filters.private & filters.user(YOUR_OWNER_ID) & ~filters.me)            
+@app.on_message(filters.private & filters.user(OWNER_ID) & ~filters.me)            
 async def forward_to_user(client: app, message: Message):
     if message.reply_to_message and message.reply_to_message.from_user.id != BOT_ID:
         await message.reply_to_message.forward(chat_id=message.reply_to_message.from_user.id)
@@ -17,7 +17,7 @@ async def forward_to_user(client: app, message: Message):
 async def forward_to_owner(client: app, message: Message):
     if message.from_user.id != BOT_ID:
         # توجيه رسالة المستخدم للمالك
-        await message.forward(chat_id=YOUR_OWNER_ID)
+        await message.forward(chat_id=OWNER_ID)
 
 if __name__ == "__main__":
     app.run(BOT_TOKEN)
